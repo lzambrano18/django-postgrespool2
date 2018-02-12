f17142b611b62d78f92cf8fce0cf75dcf16e5cc7
Leonel Zambrano <lezy17@gmail.com>
2018-02-12 09:19:38 -0500

    Fix comma

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup
-from django_postgrespool2 import __version__,
+from django_postgrespool2 import __version__
 
 required = [
     'sqlalchemy'
